f789c2a03fbee923e0e9a271bd42f8aa2706e96c
Andres-Rincon0 <andresrincondiaz3@gmail.com>
2023-08-15 09:18:45 -0500

    feat: :sparkles: prueba commit
    probando commit

--- intro-typescript/src/7mo.ts
+++ intro-typescript/src/7mo.ts
@@ -1,6 +1,6 @@
 // 7. Programa que pida el ingreso del nombre y precio de un artículo y la
 // cantidad que lleva el cliente. Mostrar lo que debe pagar el comprador
-// en su factura.
+// en su factura. 
 
 
 let nombre:string = "Andres";
